2565b45c934ef614a763d958f176855b4abc96fa
siyah <sight41siyah@gmail.com>
2022-06-27 00:22:07 +0900

    백준 10989 문제

---
+++ BJO-10989.py
@@ -0,0 +1,15 @@
+# 수 정렬하기 3
+import sys
+n = int(sys.stdin.readline())
+
+arr = [0]*10000
+for _ in range(n):
+    num = int(sys.stdin.readline()) 
+    arr[num] = arr[num] + 1
+
+for i in range(10000):
+    if arr[i] != 0:
+        for _ in range(arr[i]):
+            print(i)
+
+# append 쓰니까 메모리 초과 떴는데 이거 왜 이러는지.. 공부해보기
