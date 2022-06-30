0b6a01880782607c0037477ecd030974ee027bc9
siyah <sight41siyah@gmail.com>
2022-06-30 17:56:25 +0900

    백준 1181 문제

--- BJO-1181.py
+++ BJO-1181.py
@@ -2,12 +2,15 @@
 
 import sys
 
-# n = int(sys.stdin.readline())
-# words =[]
-# for i in range(n):
-#     word = sys.stdin.readline()
-#     if i == 0: words.append(word)
-#     else : 
-#         if len(word) < len(words[i-1]): words.insert(i-1, word)
-#         elif len(word) == len(words[i-1]): 
-#             if word[0] > words[i-1][0] : 
+n = int(sys.stdin.readline())
+
+words = []
+for _ in range(n) :
+    word = sys.stdin.readline().strip()
+    if word not in words:
+        words.append(word)
+    
+
+words.sort()
+words.sort(key = lambda x : len(x))
+for i in words: print(i)
