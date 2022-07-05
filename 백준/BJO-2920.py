6d7f26d466a55a36028bdeaa43e4fcc5ecd90d69
SiwonHwang <sight41siyah@gmail.com>
2022-07-05 23:13:27 +0900

    백준 2920 문제

--- 백준/BJO-2920.py
+++ 백준/BJO-2920.py
@@ -1,1 +1,9 @@
 #  음계
+import sys
+
+nums = sys.stdin.readline().split()
+numSor = sorted(nums)
+numRe = sorted(nums, reverse=True)
+if nums == numSor :print('ascending')
+elif nums == numRe:print('descending')
+else : print('mixed')
