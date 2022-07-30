5c2cde457bb6ad0b77da292a22922f0fe2425458
SiwonHwang <sight41siyah@gmail.com>
2022-07-30 19:36:59 +0900

    백준 2711 문제

--- 백준/BJO-2711.py
+++ 백준/BJO-2711.py
@@ -1,9 +1,8 @@
 # 오타맨 고창영
-# import sys
-# n = int(sys.stdin.readline())
-# for _ in range(n):
-#     command = sys.stdin.readline().split()
-#     num = int(command[0])
-#     word = command[1]
-#     answer = ''.join(x for x in word if x not in word[num-1])
-#     print(answer)
+import sys
+n = int(sys.stdin.readline())
+for _ in range(n):
+    num,word = sys.stdin.readline().split()
+    num = int(num); word = list(word)
+    word.pop(num-1)
+    print(''.join(word))
