a0f1595f6bb215097b306e01c9a1b00ff26aaf7a
SiwonHwang <sight41siyah@gmail.com>
2022-08-01 19:14:08 +0900

    백준 2839 문제

---
+++ 백준/BJ-2775.py
@@ -0,0 +1,11 @@
+# 부녀회장이 될테야
+# n = int(input())
+# for _ in range(n):
+#     sum = 0
+#     floor = int(input())
+#     room = int(input())
+#     for _ in range(floor):
+#         for i in range(1,room+1): 
+#             sum += i
+#             print('sum = ',sum)
+#     print(sum)
--- 백준/BJO-2839.py
+++ 백준/BJO-2839.py
@@ -1,1 +1,12 @@
 # 설탕 배달
+n = int(input())
+def calc_sugar(n):
+    sum = 0
+    while n >= 0:
+        if n % 5 == 0 : 
+            sum += n // 5
+            return sum
+        n -= 3
+        sum += 1
+    return -1
+print(calc_sugar(n))
