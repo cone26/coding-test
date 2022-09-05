69b9150d2589c961c3d2b4a4fb79792a54e5dd41
SiwonHwang <sight41siyah@gmail.com>
2022-09-05 14:14:11 +0900

    백준 1157 문제 re

--- 백준/BJO-1157.py
+++ 백준/BJO-1157.py
@@ -1,11 +1,16 @@
 # 단어 공부
 import sys 
-word = sys.stdin.readline().strip().lower()
-wordSet = list(set(word))
-cnt = []
-for i in wordSet:
-    count = word.count(i)
-    cnt.append(count)
+# word = sys.stdin.readline().strip().lower()
+# wordSet = list(set(word))
+# cnt = []
+# for i in wordSet:
+#     count = word.count(i)
+#     cnt.append(count)
 
-if cnt.count(max(cnt)) >= 2 : print('?')
-else : print(wordSet[(cnt.index(max(cnt)))].upper())
+# if cnt.count(max(cnt)) >= 2 : print('?')
+# else : print(wordSet[(cnt.index(max(cnt)))].upper())
+from collections import Counter
+word = sys.stdin.readline().strip().lower()
+common = Counter(word).most_common()
+if len(common) >= 2 and common[0][1] == common[1][1]: print('?')
+else : print(common[0][0].upper())
