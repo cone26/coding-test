27ccd837d2e356594ec135f0b14fc6a763264101
SiwonHwang <sight41siyah@gmail.com>
2022-09-13 23:50:36 +0900

    백준 11942 문제

--- 백준/BJ-1260.py
+++ 백준/BJ-1260.py
@@ -1,1 +1,35 @@
 # dfs 와 bfs
+from collections import defaultdict
+from dis import disco
+
+
+N,M,V = map(int,input().split())
+graph = defaultdict(list)
+for _ in range(M):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def recursive_dfs(v,discoverd = []):
+    discoverd.append(v)
+    for w in graph[v]:
+        if w not in discoverd:
+            discoverd = recursive_dfs(w)
+    return discoverd
+
+dfs = recursive_dfs(V)
+print(*dfs)
+
+def iterative_bfs(start_v):
+    discoverd = [start_v]
+    queue = [start_v]
+    while queue :
+        v = queue.pop(0)
+        for w in graph[v]:
+            if w not in discoverd:
+                discoverd.append(w)
+                queue.append(w)
+    return discoverd
+
+bfs = iterative_bfs(V)
+print(*bfs)
